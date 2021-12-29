1588df206044253692a3f08f47d4e0aa89b36258
farid98 <farid98@gmail.com>
2021-12-29 15:07:32 +0500

    0.1 first commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,56 @@
-import Head from 'next/head'
+import { useState, useEffect } from 'react';
+import { getSunrise, getSunset } from 'sunrise-sunset-js';
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+const Home = () => {
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+  const [sunSet, setSunSet] = useState();
+  const [sunRise, setSunRise] = useState();
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+  useEffect(() => {
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+    calculateSun()
+
+  }, []);
+
+
+  useEffect(() => {
+    
+    console.log(sunSet)
+
+  }, [sunSet]);
+
+  const calculateSun = () => {
+
+
+    const x = getSunset(33.6844, 73.0479);
+    const y = getSunrise(33.6844, 73.0479);
+
+    setSunSet(x)
+    setSunRise(y)
+
+
+  }
+
+
+  if (!sunSet || !sunRise) return (
+    <div className="flex">Loading...</div>
+
   )
+
+
+  return (
+    <div>
+      <div className="flex">Here's the sunrise and set times in Islamabad</div>
+      <div className="flex text-2xl p-6">Sun-rise: {sunRise.getHours() } : {sunRise.getMinutes()} : {sunRise.getSeconds() } </div>
+      <div className="flex text-2xl p-6">Sun-set: {sunSet.getHours() } : {sunSet.getMinutes()} : {sunSet.getSeconds() } </div>
+
+    </div>
+
+  );
 }
+
+export default Home;
